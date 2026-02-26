202f4e716f5b1a01fa4e4e114f5829334b78bff6
Meshack Mbuvi <meshmbuvi@gmail.com>
2026-02-26 08:23:30 +0300

    Add whatsapp floating button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { WhatsAppFloatingButton } from "./components/ui/WhatsAppFloatingButton";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 text-black font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         The Frontend app for Visum Park Hotels
       </main>
+      <WhatsAppFloatingButton />
     </div>
   );
 }
